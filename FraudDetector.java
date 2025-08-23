96e24586dd0477a7a9f96dacb27ea8f80e15f8a9
Diasb4 <bbdng6@gmail.com>
2025-08-23 21:57:42 +0500

    Detection of Pokemon-Fraud

--- FraudDetector.java
+++ FraudDetector.java
@@ -17,6 +17,9 @@ public class FraudDetector {
             if (tx.getAmount() > 10000) {
                 System.out.println("Fraud detected: " + tx);
             }
+            if (tx.getTrader().getName().equals("Pokemon")) {
+                System.out.println("Fraud detected: " + tx);
+            }
         }
     }
 
@@ -25,12 +28,15 @@ public class FraudDetector {
 
         Trader trader1 = new Trader("Alice", "New York");
         Trader trader2 = new Trader("Bob", "Los Angeles");
+        Trader trader3 = new Trader("Pokemon", "Pallet Town");
 
         Transaction tx1 = new Transaction(trader1, 5000);
         Transaction tx2 = new Transaction(trader2, 15000);
+        Transaction tx3 = new Transaction(trader3, 2000);
 
         detector.addTransaction(tx1);
         detector.addTransaction(tx2);
+        detector.addTransaction(tx3);
 
         detector.detectFraud();
     }
